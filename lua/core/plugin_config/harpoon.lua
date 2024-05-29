c936749c7af6279af545be2eb278ad81c779e4c3
Subash <subash@smartmca.com>
2024-05-29 15:42:40 -0400

    remove for harpppppppppppppppooooooon

--- lua/core/plugin_config/harpoon.lua
+++ lua/core/plugin_config/harpoon.lua
@@ -5,7 +5,9 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+--harpoon list remove
+vim.keymap.set("n", "<leader>hr", function() harpoon:list():remove() end)
+vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
